0cbe550a7180a40baf4401d4653c20489733c5ed
Sachuman <sasajain@ucsc.edu>
2025-02-14 10:55:21 -0800

    feature-add-footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portfolio from './Portfolio'
 import Home from './Home';
 import Blog from './Blog'
 import './App.css'
+import Footer from './Footer';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/blog" element={<Blog />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
